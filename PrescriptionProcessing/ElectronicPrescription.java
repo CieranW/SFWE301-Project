bd367c0936cc1641f2e8380517e34c19fd701ef6
meganzupancic <meganzupancic@arizona.edu>
2024-12-02 21:52:43 -0700

    Update ElectronicPrescription.java

--- PrescriptionProcessing/ElectronicPrescription.java
+++ PrescriptionProcessing/ElectronicPrescription.java
@@ -2,7 +2,11 @@ package PrescriptionProcessing;
 
 public class ElectronicPrescription {
 
-    private
+    private int prescriptionId;
+    private int medicationId;
+    private int patientId;
+    private String dosage;
+    private String notes;
 
     //import electronic prescriptions (8.3.2)
     public void importElectronicPrescription(ElectronicPrescription electronicPrescription) {
